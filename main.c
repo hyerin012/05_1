86a4797e469dada9928ac6dd8b7dab773fe29a0c
hyerin012 <70803588+hyerin012@users.noreply.github.com>
2020-10-06 16:29:04 +0900

    04 exercise

--- main.c
+++ main.c
@@ -5,21 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	int num=0;
-	char c;
+	int i,n;
+	int sum=0;
 	
-	printf("input a string:");
+	printf("input a number:");
+	scanf("%i",&n);
 	
-	while((c=getchar())!='\n')
-	{
-		if(c>='0'&&c<='9')
-		{
-			num++;
-		}
-	}
-	
-	printf("the number of digit is %i\n",num);
-	
+	for(i=1;i<=n;i++)
+	   sum +=i;//summation
+	   
+	   
+    printf("the sum result is %i\n",sum);	   
  
 	return 0;
 }
